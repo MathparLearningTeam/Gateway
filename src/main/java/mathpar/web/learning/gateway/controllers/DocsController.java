e047803381d39f51ff61c31c9368dbb1a655a74d
Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
2020-04-06 19:20:47 +0300

    Removed proxy logic; Refactored pom.xml (#5)

--- src/main/java/mathpar/web/learning/gateway/controllers/DocsController.java
+++ src/main/java/mathpar/web/learning/gateway/controllers/DocsController.java
@@ -2,13 +2,23 @@ package mathpar.web.learning.gateway.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import mathpar.web.learning.gateway.utils.MathparProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 public class DocsController {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final MathparProperties mathparProperties;
+
+    public DocsController(MathparProperties mathparProperties) {
+        this.mathparProperties = mathparProperties;
+    }
+
     @GetMapping("/swagger-resources")
     public List<SwaggerResource> getResources(){
         return List.of(
@@ -25,6 +35,16 @@ public class DocsController {
     @GetMapping("/swagger-resources/configuration/ui")
     public String getUiProperties(){
         return "{\"deepLinking\":true,\"displayOperationId\":false,\"defaultModelsExpandDepth\":1,\"defaultModelExpandDepth\":1,\"defaultModelRendering\":\"example\",\"displayRequestDuration\":false,\"docExpansion\":\"none\",\"filter\":false,\"operationsSorter\":\"alpha\",\"showExtensions\":false,\"tagsSorter\":\"alpha\",\"validatorUrl\":\"\",\"apisSorter\":\"alpha\",\"jsonEditor\":false,\"showRequestHeaders\":false,\"supportedSubmitMethods\":[\"get\",\"put\",\"post\",\"delete\",\"options\",\"head\",\"patch\",\"trace\"]}";
+    }
+
+    @GetMapping(path = "/account/docs")
+    public ResponseEntity<Object> proxyAccountDocsRequest(){
+        return restTemplate.getForEntity(mathparProperties.getAccountModulePrefix()+"/v2/api-docs", Object.class);
+    }
+
+    @GetMapping(path = "/school/docs")
+    public ResponseEntity<Object> proxySchoolDocsRequest(){
+        return restTemplate.getForEntity(mathparProperties.getSchoolModulePrefix()+"/v2/api-docs", Object.class);
     }
 
     @Getter
